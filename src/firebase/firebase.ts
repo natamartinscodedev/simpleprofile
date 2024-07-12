ec77f383739a00c7ca1e50ed1e764fb9e0403328
natamartinscodedev <nata.codedev@gmail.com>
2024-07-12 05:54:31 -0300

    correct firebase

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.PUBLIC_FIREBASE_API_KEY || 'mock_key',
-    authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN || 'mock_key',
-    projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID || 'mock_key',
-    storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET || 'mock_key',
-    messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID || 'mock_key',
-    appId: process.env.PUBLIC_FIREBASE_APP_ID || 'mock_key',
+    apiKey: process.env.NEXT_FIREBASE_API_KEY || 'mock_key',
+    authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN || 'mock_key',
+    projectId: process.env.NEXT_FIREBASE_PROJECT_ID || 'mock_key',
+    storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET || 'mock_key',
+    messagingSenderId: process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID || 'mock_key',
+    appId: process.env.NEXT_FIREBASE_APP_ID || 'mock_key',
+    measurementId: "G-JS5BSW4BWN" || 'mock_key',
 };
 
 const app = initializeApp(firebaseConfig);
