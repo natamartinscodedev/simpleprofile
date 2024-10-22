6b82b02febf0f185a44f0b41bc6488fe5be76539
natamartinscodedev <nata.codedev@gmail.com>
2024-10-22 19:16:49 -0300

    correct error in i18n

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
-import createMiddleware from 'next-intl/middleware';
+// import createMiddleware from 'next-intl/middleware';
 
-export default createMiddleware({
-  locales: ['en-US', 'pt-BR'],
+// export default createMiddleware({
+//   locales: ['en-US', 'pt-BR'],
 
-  defaultLocale: 'pt-BR',
-  localeDetection: true
-});
+//   defaultLocale: 'pt-BR',
+//   localeDetection: true
+// });
 
-export const config = {
-  // matcher: ['/', '/(en-US|pt-BR)/:path*']
-  matcher: ['/((?!api|_next|.*\\..*).*)']
-};
+// export const config = {
+//   // matcher: ['/', '/(en-US|pt-BR)/:path*']
+//   matcher: ['/((?!api|_next|.*\\..*).*)']
+// };
 
