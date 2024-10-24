8e4eecb58a924c0f3a308274b44376e05dde50c1
natamartinscodedev <nata.codedev@gmail.com>
2024-10-24 16:56:22 -0300

    correct erro deploy env

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,7 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
     'STRIPE_SECRET_KEY is missing. Please set the environment variable.'
   )
 }
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: '2024-09-30.acacia'
 })
