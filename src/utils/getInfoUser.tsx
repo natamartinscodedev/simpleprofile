edb260a4562e817963a05f81b9ec8ad6926d651d
natamartinscodedev <nata.codedev@gmail.com>
2024-11-11 11:34:50 -0300

    correct load user shared

--- src/utils/getInfoUser.tsx
+++ src/utils/getInfoUser.tsx
@@ -1,19 +1,29 @@
-import { fetchMongodb } from "./fetchMongodb";
+import { fetchMongodb } from './fetchMongodb'
 
-export async function GetDataUser(email?: any, nameLink?: any) {
+interface TypeParams {
+  email?: any,
+  nameLink?: any
+}
+
+export async function GetDataUser({
+                                    email, nameLink
+                                  }: TypeParams) {
+  console.log('User ===>', nameLink)
   try {
-    const { topics } = await fetchMongodb();
+    const { topics } = await fetchMongodb()
     if (topics) {
       const res = topics.filter(
-        (user: any) => user.email === email || user.nameLink === nameLink
-      );
-      const User = res[0];
+        // (user: any) => user.email === email || user.nameLink === nameLink
+        (user: any) => user.nameLink === nameLink
+      )
+      const User = res[0]
 
-      return { User };
+
+      return { User }
     } else {
-      console.log("Topics don't exist");
+      console.log('Topics don\'t exist')
     }
   } catch (err) {
-    console.log("Erro:", err);
+    console.log('Erro:', err)
   }
 }
