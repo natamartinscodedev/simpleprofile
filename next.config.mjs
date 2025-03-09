0f4c2e34169484074af0cbca55b6a090e4727865
natamartinscodedev <nata.codedev@gmail.com>
2025-03-08 22:26:17 -0300

    add corrections erros in18

--- next.config.mjs
+++ next.config.mjs
@@ -1,44 +1,35 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 const nextConfig = {
-  // reactStrictMode: true,
   webpack: (config, { isServer }) => {
-    config.resolve.alias['aws-s3-upload-ash'] =
-      require.resolve('aws-s3-upload-ash')
-    return config
+    // Cria um alias para o módulo 'aws-s3-upload-ash'
+    config.resolve.alias['aws-s3-upload-ash'] = require.resolve('aws-s3-upload-ash');
+    return config;
   },
   images: {
+    domains: ['firebasestorage.googleapis.com'],
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '*.ico'
-      },
-      {
-        protocol: 'https',
-        hostname: '*.com'
-      },
-      {
-        protocol: 'https',
-        hostname: '*.svg'
-      },
-      {
-        protocol: 'https',
-        hostname: '*.png'
-      }
+      { protocol: 'http', hostname: '*.ico' },
+      { protocol: 'https', hostname: '*.com' },
+      { protocol: 'https', hostname: '*.svg' },
+      { protocol: 'https', hostname: '*.png' },
     ],
-    minimumCacheTTL: 1500000
+    minimumCacheTTL: 1500000,
   },
   turbo: {
     swcMinify: true,
-    optimizeFonts: true
+    optimizeFonts: true,
   },
   async rewrites() {
     return [
       {
         source: '/linkPersonalize',
-        destination: '/Components/CreateSingUp/Index' // ou 'pages/login'
-      }
-    ]
+        destination: '/Components/CreateSingUp/Index', // ou 'pages/login'
+      },
+    ];
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
