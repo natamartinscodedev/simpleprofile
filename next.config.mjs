05bd089459f621c850ecb47b7d730835e2610a48
natamartinscodedev <nata.codedev@gmail.com>
2025-03-08 22:32:45 -0300

    add corrections erros in18

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,11 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Cria um alias para o módulo 'aws-s3-upload-ash'
-    config.resolve.alias['aws-s3-upload-ash'] = require.resolve('aws-s3-upload-ash');
-    return config;
-  },
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
-    remotePatterns: [
-      { protocol: 'http', hostname: '*.ico' },
-      { protocol: 'https', hostname: '*.com' },
-      { protocol: 'https', hostname: '*.svg' },
-      { protocol: 'https', hostname: '*.png' },
-    ],
-    minimumCacheTTL: 1500000,
-  },
+  // webpack: (config, { isServer }) => {
+  //   // Cria um alias para o módulo 'aws-s3-upload-ash'
+  //   config.resolve.alias['aws-s3-upload-ash'] = require.resolve('aws-s3-upload-ash');
+  //   return config;
+  // },
   turbo: {
     swcMinify: true,
     optimizeFonts: true,
