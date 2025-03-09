a4ebc75be99a2fcd043519fb59839c82259cb74d
natamartinscodedev <nata.codedev@gmail.com>
2025-03-08 23:32:24 -0300

    correct erro

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
   )
 }
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-09-30.acacia"
+  apiVersion: "2025-02-24.acacia"
 })
 
 export default stripe
