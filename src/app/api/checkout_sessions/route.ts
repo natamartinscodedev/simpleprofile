2cbcc2e34c4c3944ec8034c711cd222dd8893358
natamartinscodedev <nata.codedev@gmail.com>
2025-03-08 23:12:31 -0300

    correct erro

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -1,6 +1,7 @@
 import stripe from '@/lib/stripe';
 import { NextRequest, NextResponse } from 'next/server';
 
+
 export async function POST(req: NextRequest) {
   const { testeId, assinatura, } = await req.json();
   // Defina o ID de preço com base no tipo de transação
@@ -8,18 +9,14 @@ export async function POST(req: NextRequest) {
     const session = await stripe.checkout.sessions.create({
       line_items: [
         {
-          price: process.env.STRIPE_SUBSCRIPTION_PRICE_ID
-          ,
+          price: process.env.STRIPE_SUBSCRIPTION_PRICE_ID as string,
           quantity: 1,
         },
       ],
-      mode: assinatura === true && 'subscription',
+      mode: 'subscription',
       payment_method_types: assinatura && ['card'],
       success_url: `${req.headers.get('origin')}/Sucesso/${assinatura}`,
       cancel_url: `${req.headers.get('origin')}/`,
-      metadata: {
-        testeId,
-      },
     });
 
     return NextResponse.json({ sessionId: session.id, ok: true });
